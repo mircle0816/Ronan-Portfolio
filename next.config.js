a65c1f62516958dbd6e9cffb9bb27759c1c70902
Ronan Gadingan Abequibel <ronanbusinessemail@gmail.com>
2026-05-14 22:52:40 -0600

    Remove Sentry

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { withSentryConfig } = require("@sentry/nextjs");
 
 const moduleExports = {
   reactStrictMode: true,
@@ -28,4 +27,4 @@ const sentryWebpackPluginOptions = {
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+module.exports = nextConfig;
